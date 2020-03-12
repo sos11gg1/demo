e2285dd0e4792bbd36353205ba815338c49d242d
methosjim <methos90328@gmail.com>
2020-03-12 20:17:22 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random i = new Random();
+            int hp = i.Next(0, 100);
+            Console.WriteLine("嗨");
+            while (true)
+            {
+                //判斷死亡
+                if (hp <= 0)
+                {
+                    Console.WriteLine("你已經死了...");
+                    Console.WriteLine("      ╱╲");
+                    Console.WriteLine("    ╱    ╲");
+                    Console.WriteLine("  ╱  <●>  ╲");
+                    Console.WriteLine("╱＿＿＿＿＿＿╲");
+                    break;
+                }
+                Console.WriteLine("請輸入數字:");
+                //檢查是否有人攻擊
+                string attackString = Console.ReadLine();
+                int attack = int.Parse(attackString);
+                //Console.WriteLine(attack);
+
+                //扣血
+                hp = hp - attack;
+            }
+            Console.ReadLine();
+        }
+    }
+}
